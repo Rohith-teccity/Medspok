efb41344fff8839e7c4c69dcaa304de921601c3d
Basith09 <basith0903@gmail.com>
2025-02-26 16:36:08 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./component/Navbar/Navbar";
 import Who from "./component/Who/Who";
+import Footer from "./component/footer/Footer";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Who />
+      <Footer />
     </>
   );
 };
