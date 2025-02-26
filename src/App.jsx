835a187e19fe8f0f277447bc565b691562602c90
Rohith-teccity <rohith@teccitylabs.com>
2025-02-26 18:31:46 +0530

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./component/Navbar/Navbar";
 import Who from "./component/Who/Who";
+import Banner from "./component/Banner/Banner";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Banner />
       <Who />
     </>
   );
---
+++ src/component/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Banner.css"; // Import the CSS file
+
+const Banner = () => {
+  return (
+    <div className="banner">
+    </div>
+  );
+};
+
+export default Banner;
